d4f3e0be011f5a475489dcc947d5b55859c40a9b
Christoffer <89570173+ChrisRuLee@users.noreply.github.com>
2021-09-10 09:51:22 +0200

    Add files via upload

---
+++ Aritmetikk/src/Aritmetikk/Aritmetikkoppg2.java
@@ -0,0 +1,12 @@
+package Aritmetikk;
+
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Aritmetikkoppg2 {
+    public static void main (String [] args) {
+        int antall = 0;
+    showMessageDialog(null, antall);
+    }
+}
